577f94b45aee9cc670deb480e147923e5ec13e90
slowdedasus <130231897+slowdedasus@users.noreply.github.com>
2023-07-20 00:42:20 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xe04086f9c643c052eca3389472c41ef81a84c9e2";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
